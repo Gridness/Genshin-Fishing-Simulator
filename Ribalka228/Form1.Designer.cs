2fec8f4fa7e4f64d7c8b3042e339582729c4537a
Gridness <zonddas@yahoo.com>
2021-10-02 14:57:09 +0300

    Poshel davai go go go

---
+++ Ribalka228/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace Ribalka228
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.patternLabel = new System.Windows.Forms.Label();
+            this.positionShiftInterval = new System.Windows.Forms.TextBox();
+            this.zoneShiftInterval = new System.Windows.Forms.TextBox();
+            this.minZone = new System.Windows.Forms.TextBox();
+            this.maxZone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.distanceError = new System.Windows.Forms.Label();
+            this.prjamougolnik = new System.Windows.Forms.Button();
+            this.fishButton = new System.Windows.Forms.Button();
+            this.fishingBar = new System.Windows.Forms.ProgressBar();
+            this.fishingDot = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dotMS = new System.Windows.Forms.TextBox();
+            this.comparisonLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(478, 250);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(156, 47);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // patternLabel
+            // 
+            this.patternLabel.AutoSize = true;
+            this.patternLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.patternLabel.Location = new System.Drawing.Point(533, 66);
+            this.patternLabel.Name = "patternLabel";
+            this.patternLabel.Size = new System.Drawing.Size(61, 20);
+            this.patternLabel.TabIndex = 2;
+            this.patternLabel.Text = "Pattern";
+            this.patternLabel.Click += new System.EventHandler(this.patternLabel_Click);
+            // 
+            // positionShiftInterval
+            // 
+            this.positionShiftInterval.Location = new System.Drawing.Point(515, 99);
+            this.positionShiftInterval.Name = "positionShiftInterval";
+            this.positionShiftInterval.Size = new System.Drawing.Size(100, 20);
+            this.positionShiftInterval.TabIndex = 3;
+            this.positionShiftInterval.TextChanged += new System.EventHandler(this.positionShiftInterval_TextChanged);
+            // 
+            // zoneShiftInterval
+            // 
+            this.zoneShiftInterval.Location = new System.Drawing.Point(515, 125);
+            this.zoneShiftInterval.Name = "zoneShiftInterval";
+            this.zoneShiftInterval.Size = new System.Drawing.Size(100, 20);
+            this.zoneShiftInterval.TabIndex = 4;
+            this.zoneShiftInterval.TextChanged += new System.EventHandler(this.zoneShiftInterval_TextChanged);
+            // 
+            // minZone
+            // 
+            this.minZone.Location = new System.Drawing.Point(515, 151);
+            this.minZone.Name = "minZone";
+            this.minZone.Size = new System.Drawing.Size(100, 20);
+            this.minZone.TabIndex = 5;
+            this.minZone.TextChanged += new System.EventHandler(this.minZone_TextChanged);
+            // 
+            // maxZone
+            // 
+            this.maxZone.Location = new System.Drawing.Point(515, 177);
+            this.maxZone.Name = "maxZone";
+            this.maxZone.Size = new System.Drawing.Size(100, 20);
+            this.maxZone.TabIndex = 6;
+            this.maxZone.TextChanged += new System.EventHandler(this.maxZone_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(392, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Position shift interval (ms)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(392, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Zone shift interval (ms)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(392, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Min zone (> 15)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(392, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Max zone";
+            // 
+            // distanceError
+            // 
+            this.distanceError.AutoSize = true;
+            this.distanceError.Font = new System.Drawing.Font("Segoe Script", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.distanceError.ForeColor = System.Drawing.Color.Red;
+            this.distanceError.Location = new System.Drawing.Point(-6, 48);
+            this.distanceError.Name = "distanceError";
+            this.distanceError.Size = new System.Drawing.Size(655, 106);
+            this.distanceError.TabIndex = 11;
+            this.distanceError.Text = "ТЫ ЕБЛАН ЧЕЕЛ";
+            this.distanceError.Visible = false;
+            // 
+            // prjamougolnik
+            // 
+            this.prjamougolnik.Location = new System.Drawing.Point(12, 12);
+            this.prjamougolnik.Name = "prjamougolnik";
+            this.prjamougolnik.Size = new System.Drawing.Size(75, 23);
+            this.prjamougolnik.TabIndex = 12;
+            this.prjamougolnik.UseVisualStyleBackColor = true;
+            this.prjamougolnik.Visible = false;
+            // 
+            // fishButton
+            // 
+            this.fishButton.Location = new System.Drawing.Point(12, 250);
+            this.fishButton.Name = "fishButton";
+            this.fishButton.Size = new System.Drawing.Size(156, 47);
+            this.fishButton.TabIndex = 13;
+            this.fishButton.Text = "Fish";
+            this.fishButton.UseVisualStyleBackColor = true;
+            this.fishButton.Visible = false;
+            this.fishButton.Click += new System.EventHandler(this.rndButton_Click);
+            // 
+            // fishingBar
+            // 
+            this.fishingBar.Location = new System.Drawing.Point(12, 12);
+            this.fishingBar.Name = "fishingBar";
+            this.fishingBar.Size = new System.Drawing.Size(622, 23);
+            this.fishingBar.TabIndex = 1;
+            this.fishingBar.Visible = false;
+            this.fishingBar.Click += new System.EventHandler(this.fishingBar_Click);
+            // 
+            // fishingDot
+            // 
+            this.fishingDot.AutoSize = true;
+            this.fishingDot.BackColor = System.Drawing.Color.Transparent;
+            this.fishingDot.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fishingDot.Location = new System.Drawing.Point(315, 12);
+            this.fishingDot.Name = "fishingDot";
+            this.fishingDot.Size = new System.Drawing.Size(14, 18);
+            this.fishingDot.TabIndex = 14;
+            this.fishingDot.Text = "•";
+            this.fishingDot.Visible = false;
+            this.fishingDot.Click += new System.EventHandler(this.fishingDot_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(392, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Dot MS";
+            // 
+            // dotMS
+            // 
+            this.dotMS.Location = new System.Drawing.Point(515, 203);
+            this.dotMS.Name = "dotMS";
+            this.dotMS.Size = new System.Drawing.Size(100, 20);
+            this.dotMS.TabIndex = 18;
+            this.dotMS.TextChanged += new System.EventHandler(this.dotMS_TextChanged);
+            // 
+            // comparisonLabel
+            // 
+            this.comparisonLabel.AutoSize = true;
+            this.comparisonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comparisonLabel.ForeColor = System.Drawing.Color.Red;
+            this.comparisonLabel.Location = new System.Drawing.Point(21, 134);
+            this.comparisonLabel.Name = "comparisonLabel";
+            this.comparisonLabel.Size = new System.Drawing.Size(100, 42);
+            this.comparisonLabel.TabIndex = 19;
+            this.comparisonLabel.Text = "OPA";
+            this.comparisonLabel.Visible = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(647, 309);
+            this.Controls.Add(this.comparisonLabel);
+            this.Controls.Add(this.dotMS);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.fishingDot);
+            this.Controls.Add(this.fishButton);
+            this.Controls.Add(this.prjamougolnik);
+            this.Controls.Add(this.distanceError);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maxZone);
+            this.Controls.Add(this.minZone);
+            this.Controls.Add(this.zoneShiftInterval);
+            this.Controls.Add(this.positionShiftInterval);
+            this.Controls.Add(this.patternLabel);
+            this.Controls.Add(this.fishingBar);
+            this.Controls.Add(this.startButton);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "Ribalka kruto pog aaa xD xD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label patternLabel;
+        private System.Windows.Forms.TextBox positionShiftInterval;
+        private System.Windows.Forms.TextBox zoneShiftInterval;
+        private System.Windows.Forms.TextBox minZone;
+        private System.Windows.Forms.TextBox maxZone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label distanceError;
+        private System.Windows.Forms.Button prjamougolnik;
+        private System.Windows.Forms.Button fishButton;
+        private System.Windows.Forms.ProgressBar fishingBar;
+        private System.Windows.Forms.Label fishingDot;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox dotMS;
+        private System.Windows.Forms.Label comparisonLabel;
+    }
+}
+
---
+++ Ribalka228/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Ribalka228
+{
+    public partial class Main : Form
+    {
+        int prX, prY, prH, prW;
+
+        int _minZone, _maxZone;
+        int _positionShiftInterval, _zoneShiftInterval;
+        int _dotMS;
+
+        Timer positionClock, zoneClock, dotMoveClock, posCompareClock;
+
+        Random rnd = new Random();
+
+        bool gameStarted = false;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            dotMS.Text = "0";
+            positionShiftInterval.Text = "0";
+            zoneShiftInterval.Text = "0";
+        }
+
+        private void InitializeTimer()
+        {
+            positionClock = new Timer();
+            zoneClock = new Timer();
+            dotMoveClock = new Timer();
+            posCompareClock = new Timer();
+
+            positionClock.Interval = _positionShiftInterval;
+            zoneClock.Interval = _zoneShiftInterval;
+            dotMoveClock.Interval = 1000;
+            posCompareClock.Interval = 1;
+
+            positionClock.Tick += new EventHandler(positionClockTick);
+            zoneClock.Tick += new EventHandler(zoneClockTick);
+            dotMoveClock.Tick += new EventHandler(moveDotClockTick);
+            posCompareClock.Tick += new EventHandler(posCompareClockTick);
+
+            positionClock.Start();
+            zoneClock.Start();
+            dotMoveClock.Start();
+            posCompareClock.Start();
+        }
+
+        private void posCompareClockTick(object sender, EventArgs e)
+        {
+            comparePositions();
+        }
+
+        private void stopTimers()
+        {
+            positionClock.Stop();
+            zoneClock.Stop();
+            dotMoveClock.Stop();
+            posCompareClock.Stop();
+        }
+
+        private void moveDotClockTick(object sender, EventArgs e)
+        {
+            moveDot(_dotMS);
+        }
+
+        private void zoneClockTick(object sender, EventArgs e)
+        {
+            changeZone(_minZone, _maxZone);
+        }
+
+        private void positionClockTick(object sender, EventArgs e)
+        {
+            changePosition();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!gameStarted)
+            {
+                prX = prjamougolnik.Location.X;
+                prY = prjamougolnik.Location.Y;
+                prH = prjamougolnik.Size.Height;
+                prW = prjamougolnik.Size.Width;
+
+                gameStarted = true;
+                startButton.Text = "Stop";
+                prjamougolnik.Visible = true;
+                fishingDot.Visible = true;
+                fishButton.Visible = true;
+                comparisonLabel.Visible = true;
+
+                minZone.Enabled = false;
+                maxZone.Enabled = false;
+                positionShiftInterval.Enabled = false;
+                zoneShiftInterval.Enabled = false;
+                dotMS.Enabled = false;
+
+                InitializeTimer();
+            }
+            else
+            {
+                gameStarted = false;
+                startButton.Text = "Start";
+                prjamougolnik.Visible = false;
+                fishingDot.Visible = false;
+                fishButton.Visible = false;
+                comparisonLabel.Visible = false;
+
+                minZone.Enabled = true;
+                maxZone.Enabled = true;
+                positionShiftInterval.Enabled = true;
+                zoneShiftInterval.Enabled = true;
+                dotMS.Enabled = true;
+
+                dropProperties(prX, prY, prH, prW);
+                stopTimers();
+            }
+        }
+
+        private void minZone_TextChanged(object sender, EventArgs e)
+        {
+            Int32.TryParse(minZone.Text, out _minZone);
+            checkDistance(_minZone, _maxZone);
+        }
+
+        private void maxZone_TextChanged(object sender, EventArgs e)
+        {
+            Int32.TryParse(maxZone.Text, out _maxZone);
+            checkDistance(_minZone, _maxZone);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // hueta ne nuzjnaja
+        }
+
+        private void positionShiftInterval_TextChanged(object sender, EventArgs e)
+        {
+            Int32.TryParse(positionShiftInterval.Text, out _positionShiftInterval);
+        }
+
+        private void zoneShiftInterval_TextChanged(object sender, EventArgs e)
+        {
+            Int32.TryParse(zoneShiftInterval.Text, out _zoneShiftInterval);
+        }
+
+        private void rndButton_Click(object sender, EventArgs e)
+        {
+            var fishingDotX = fishingDot.Location.X;
+            var fishingDotY = fishingDot.Location.Y;
+
+            fishingDotX += _dotMS * 2;
+
+            fishingDot.Location = new Point(fishingDotX, fishingDotY);
+        }
+
+        private void checkDistance(int _minZone, int _maxZone)
+        {
+            if(_minZone >= _maxZone)
+            {
+                distanceError.Visible = true;
+                startButton.Enabled = false;
+                startButton.Text = "ТЫ ЕБЛАН, ЧЕЕЛ";
+                startButton.ForeColor = Color.Red;
+            } else
+            {
+                distanceError.Visible = false;
+                startButton.Enabled = true;
+                startButton.Text = "Start";
+                startButton.ForeColor = Color.Black;
+            }
+
+            checkMinValue(_minZone);
+        }
+
+        private void checkMinValue(int minValue)
+        {
+            if(minValue < 15)
+            {
+                startButton.Enabled = false;
+            }
+        }
+
+        private void fishingBar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dotMS_TextChanged(object sender, EventArgs e)
+        {
+            Int32.TryParse(dotMS.Text, out _dotMS);
+        }
+
+        private void patternLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fishingDot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void changePosition()
+        {
+            var prX = prjamougolnik.Location.X;
+            var prY = prjamougolnik.Location.Y;
+
+            prX = rnd.Next(fishingBar.Location.X, fishingBar.Location.X + fishingBar.Size.Width - prjamougolnik.Size.Width);
+            prjamougolnik.Location = new Point(prX, prY);
+        }
+
+        private void changeZone(int _minZone, int _maxZone)
+        {
+            var sizePIDOORX = prjamougolnik.Size.Width;
+            var sizePIDOORY = prjamougolnik.Size.Height;
+
+            sizePIDOORX = rnd.Next(_minZone, _maxZone);
+            prjamougolnik.Size = new Size(sizePIDOORX, sizePIDOORY);
+        }
+
+        private void dropProperties(int x, int y, int height, int width)
+        {
+            prjamougolnik.Location = new Point(x, y);
+            prjamougolnik.Size = new Size(width, height);
+        }
+
+        private void moveDot(int MS)
+        {
+            var fishingDotX = fishingDot.Location.X;
+            var fishingDotY = fishingDot.Location.Y;
+
+            fishingDotX -= MS;
+
+            fishingDot.Location = new Point(fishingDotX, fishingDotY);
+        }
+
+        private void comparePositions()
+        {
+            if(fishingDot.Location.X >= prjamougolnik.Location.X && fishingDot.Location.X <= prX + prjamougolnik.Size.Width - fishingDot.Size.Width)
+            {
+                comparisonLabel.Text = "Нихуя ты гений";
+                comparisonLabel.ForeColor = Color.Green;
+            } else
+            {
+                comparisonLabel.Text = "Слабоват";
+                comparisonLabel.ForeColor = Color.Red;
+            }
+        }
+    }
+}
